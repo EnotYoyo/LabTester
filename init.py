73442ead1d20085325a22ef05d4c4f4124502bfb
Enot <enot@scryptmail.com>
2016-12-11 22:13:11 +0300

    Update

--- init.py
+++ init.py
@@ -3,8 +3,9 @@ from PyQt5.QtGui import *
 from PyQt5 import QtCore
 from PyQt5.QtCore import Qt
 import form, sys
+from collections import OrderedDict
 
-json_object = {}
+json_object = OrderedDict()
 
 class functions():
     # test, label, name, tests, labs
@@ -69,6 +70,7 @@ class functions():
         if len(text) == 0 or count == 0:
             return
 
+        print(self.json_object)
         if not(text in self.json_object[self.object[4].selectedIndexes()[0].data()]):
             self.object[3].model().appendRow(functions.create_item(text))
         test = []
@@ -84,6 +86,7 @@ class functions():
         self.object[1].clear()
 
 class formManager():
+
     def __init__(self, widget):
         self.mainWindow = form.Ui_MainWindow()
         self.mainWindow.setupUi(MainWindow=widget)
@@ -102,16 +105,33 @@ class formManager():
     # callback
     def clear_labs(self):
         self.mainWindow.t_labs.model().clear()
-        self.json_object = {}
+        self.mainWindow.m_tests.model().clear()
+        self.mainWindow.a_tests.model().clear()
+        self.m_functions.clear()
+        self.a_functions.clear()
+        self.json_object = OrderedDict()
 
     # callback
     def add_lab(self):
         text = self.mainWindow.t_label.text()
         if len(text) == 0 or text in self.json_object:
             return
-        self.json_object[text] = {}
+        self.json_object[text] = OrderedDict()
         self.mainWindow.t_labs.model().appendRow(functions.create_item(text))
         self.mainWindow.t_label.clear()
+
+    def clear_selections(self):
+        self.mainWindow.a_tests.clearSelection()
+        self.mainWindow.m_tests.clearSelection()
+        self.mainWindow.t_tests.clearSelection()
+        self.m_functions.clear()
+        self.a_functions.clear()
+        for index in self.mainWindow.a_labs.selectedIndexes():
+            self.a_functions.clicked_labs(index)
+        for index in self.mainWindow.m_labs.selectedIndexes():
+            self.m_functions.clicked_labs(index)
+        for index in self.mainWindow.t_labs.selectedIndexes():
+            self.clicked(index)
 
     def all_init(self):
         # create all models
@@ -125,29 +145,30 @@ class formManager():
         self.mainWindow.m_labs.setModel(labModels)
         self.mainWindow.a_labs.setModel(labModels)
 
-        m_functions = functions(self.mainWindow.m_test, self.mainWindow.m_label,
+        self.m_functions = functions(self.mainWindow.m_test, self.mainWindow.m_label,
                                 self.mainWindow.m_name, self.mainWindow.m_tests, self.mainWindow.m_labs)
-        a_functions = functions(self.mainWindow.a_test, self.mainWindow.a_label,
+        self.a_functions = functions(self.mainWindow.a_test, self.mainWindow.a_label,
                                 self.mainWindow.a_name, self.mainWindow.a_tests, self.mainWindow.a_labs)
 
         # add functions for key-delete and esc
-        self.mainWindow.m_test.keyPressEvent = m_functions.key_press
-        self.mainWindow.a_test.keyPressEvent = a_functions.key_press
-        self.mainWindow.m_tests.keyPressEvent = m_functions.key_press_tests
-        self.mainWindow.a_tests.keyPressEvent = a_functions.key_press_tests
+        self.mainWindow.m_test.keyPressEvent = self.m_functions.key_press
+        self.mainWindow.a_test.keyPressEvent = self.a_functions.key_press
+        self.mainWindow.m_tests.keyPressEvent = self.m_functions.key_press_tests
+        self.mainWindow.a_tests.keyPressEvent = self.a_functions.key_press_tests
         self.mainWindow.t_labs.keyPressEvent = functions(self.mainWindow.t_labs).key_press
 
         # clicked callback
         self.mainWindow.t_labs.selectionModel().currentChanged.connect(self.clicked)
-        self.mainWindow.a_labs.selectionModel().currentChanged.connect(a_functions.clicked_labs)
-        self.mainWindow.m_labs.selectionModel().currentChanged.connect(m_functions.clicked_labs)
-        self.mainWindow.a_tests.selectionModel().currentChanged.connect(a_functions.clicked_tests)
-        self.mainWindow.m_tests.selectionModel().currentChanged.connect(m_functions.clicked_tests)
-        self.mainWindow.a_tests.clicked[QtCore.QModelIndex].connect(a_functions.clicked_tests)
-        self.mainWindow.m_tests.clicked[QtCore.QModelIndex].connect(m_functions.clicked_tests)
+        self.mainWindow.a_labs.selectionModel().currentChanged.connect(self.a_functions.clicked_labs)
+        self.mainWindow.m_labs.selectionModel().currentChanged.connect(self.m_functions.clicked_labs)
+        self.mainWindow.a_tests.selectionModel().currentChanged.connect(self.a_functions.clicked_tests)
+        self.mainWindow.m_tests.selectionModel().currentChanged.connect(self.m_functions.clicked_tests)
+        self.mainWindow.a_tests.clicked[QtCore.QModelIndex].connect(self.a_functions.clicked_tests)
+        self.mainWindow.m_tests.clicked[QtCore.QModelIndex].connect(self.m_functions.clicked_tests)
         self.mainWindow.t_labs.clicked[QtCore.QModelIndex].connect(self.clicked)
-        self.mainWindow.a_labs.clicked[QtCore.QModelIndex].connect(a_functions.clicked_labs)
-        self.mainWindow.m_labs.clicked[QtCore.QModelIndex].connect(m_functions.clicked_labs)
+        self.mainWindow.a_labs.clicked[QtCore.QModelIndex].connect(self.a_functions.clicked_labs)
+        self.mainWindow.m_labs.clicked[QtCore.QModelIndex].connect(self.m_functions.clicked_labs)
+        self.mainWindow.tabWidget.tabBarClicked.connect(self.clear_selections)
 
         # add validators
         validator = QtCore.QRegExp('[><][\w\s\[\]\,\(\)\.]{1,}$')
@@ -156,18 +177,18 @@ class formManager():
         self.mainWindow.a_label.setValidator(QRegExpValidator(validator))
 
         # when press return, then adds new element in list
-        self.mainWindow.m_label.returnPressed.connect(m_functions.label_return)
-        self.mainWindow.a_label.returnPressed.connect(a_functions.label_return)
+        self.mainWindow.m_label.returnPressed.connect(self.m_functions.label_return)
+        self.mainWindow.a_label.returnPressed.connect(self.a_functions.label_return)
         self.mainWindow.t_label.returnPressed.connect(self.add_lab)
 
         # add callback for save buttons
-        self.mainWindow.m_save.pressed.connect(m_functions.save_pressed)
-        self.mainWindow.a_save.pressed.connect(a_functions.save_pressed)
+        self.mainWindow.m_save.pressed.connect(self.m_functions.save_pressed)
+        self.mainWindow.a_save.pressed.connect(self.a_functions.save_pressed)
         self.mainWindow.t_add.pressed.connect(self.add_lab)
 
         # add callback for clear buttons
-        self.mainWindow.m_clear.pressed.connect(m_functions.clear)
-        self.mainWindow.a_clear.pressed.connect(a_functions.clear)
+        self.mainWindow.m_clear.pressed.connect(self.m_functions.clear)
+        self.mainWindow.a_clear.pressed.connect(self.a_functions.clear)
         self.mainWindow.t_clear.pressed.connect(self.clear_labs)
 
 if __name__ == '__main__':
